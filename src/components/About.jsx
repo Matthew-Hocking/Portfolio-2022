d7eaf72e6d6ad73801932e234934a6d6f3814ff5
matthew-hocking <matthocking2439@gmail.com>
2022-04-07 22:14:35 +1200

    about background image and skills items complete

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,14 +4,14 @@ import Title from './Title'
 
 const About = () => {
   return (
-      <div className='container' id="about">
-        <Title prompt="Learn a bit" title="About me" />
-        <div className='about__profile'>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-          </p>
-        </div>
+    <div className='container bg-image about' id="about">
+      <Title prompt="Learn a bit" title="About me" />
+      <div className='about__profile'>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+        </p>
       </div>
+    </div>
   )
 }
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,10 +1,57 @@
 import React from 'react'
+
 import Title from './Title'
 
 const Skills = () => {
   return (
     <div className='container' id="skills">
-      <Title prompt="Go through" title="My skills"/>
+      <Title prompt="Go through" title="My skills" />
+      <div className='skills__card'>
+        <div className='skills__item'>
+          <img src="images/javascript.svg" alt="javascript" />
+          <p>JavaScript</p>
+        </div>
+        <div className='skills__item'>
+          <img src="images/html5.svg" alt="html5" />
+          <p>HTML5</p>
+        </div>
+        <div className='skills__item'>
+          <img src="images/css3.svg" alt="css3" />
+          <p>CSS3</p>
+        </div>
+        <div className='skills__item'>
+          <img src="images/sass.svg" alt="sass" />
+          <p>SASS</p>
+        </div>
+        <div className='skills__item'>
+          <img src="images/node.svg" alt="node" />
+          <p>Node</p>
+        </div>
+        <div className='skills__item'>
+          <img src="images/react.svg" alt="react" />
+          <p>React</p>
+        </div>
+        <div className='skills__item'>
+          <img src="images/material-ui.svg" alt="material-ui" />
+          <p>Material-UI</p>
+        </div>
+        <div className='skills__item'>
+          <img src="images/git.svg" alt="git" />
+          <p>Git</p>
+        </div>
+        <div className='skills__item'>
+          <img src="images/sqlite.svg" alt="sqlite" />
+          <p>SQLite</p>
+        </div>
+        <div className='skills__item'>
+          <img src="images/mongodb.svg" alt="mongo db" />
+          <p>MongoDB</p>
+        </div>
+        <div className='skills__item'>
+          <img src="images/sanity.svg" alt="sanity" />
+          <p>Sanity CMS</p>
+        </div>
+      </div>
     </div>
   )
 }
