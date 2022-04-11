0737fda2d420133002640a9972119f3d79af140e
matthew-hocking <matthocking2439@gmail.com>
2022-04-11 18:11:42 +1200

    init framer motion for projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from './Title'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 
 import projectsData from '../data/projects'
 
-const projects = () => {
+const Projects = () => {
+
+  const [selected, setSelected] = useState(false)
+
   return (
     <div className='container' id="projects">
       <Title prompt="Check out" title="My Projects" />
-      <div className='skills__card'>
 
+      <div className='projects__wrapper'>
+        {projectsData.map((project, id) => (
+          <motion.div className='projects__card' layoutId={id} onClick={() => setSelected(id)}>
+            <motion.h1>{project.name}</motion.h1>
+            {selected && (
+              <motion.div layoutId={selected}>
+                <motion.p>{project.description}</motion.p>
+                <motion.h1 onClick={() => setSelected(null)} >X</motion.h1>
+              </motion.div>
+            )}
+          </motion.div>
+        ))}
       </div>
+
     </div>
   )
 }
 
-export default projects
+export default Projects
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,6 @@
 export default [
   {
+    id: 1,
     name: "Share-Me",
     deployLink: "",
     deployMethod: "",
@@ -8,6 +9,7 @@ export default [
     tools: "React, Sanity CMS, Node.js"
   },
   {
+    id: 2,
     name: "Gericht",
     deployLink: "",
     deployMethod: "",
@@ -16,6 +18,7 @@ export default [
     tools: "React, Node.js"
   },
   {
+    id: 3,
     name: "Checkd",
     deployLink: "",
     deployMethod: "",
@@ -24,6 +27,7 @@ export default [
     tools: ""
   },
   {
+    id: 4,
     name: "Hihi",
     deployLink: "https://hihi-app.herokuapp.com/#/",
     deployMethod: "HerokuApp",
@@ -31,21 +35,25 @@ export default [
     description: "Hihi is a social media app heavily inspired by Twitter. A group project in the final week of Bootcamp at Enspiral Development Academy as a culmination of efforts and learnings, heavily built using React + Redux."
   },
   {
+    id: 5,
     name: "Lost and Found",
     github: "https://github.com/Matthew-Hocking/Lost-and-Found",
     description: "A 2 day group project at Enspiral Development Academy, this is A digital bilboard app allowing registered users to make posts for lost or found pets, users can search and commicate to bring pets back to their loved ones."
   },
   {
+    id: 6,
     name: "Goodbye Fresh",
     github: "https://github.com/Matthew-Hocking/Goodbye-Fresh",
     description: "An app that provides a random take-out option for users finding difficulty in picking dinner, using the Nearby Search API within the Google Maps Places API for all GET & POST routes. This was a 1 day group project at Enspiral Development Academy."
   },
   {
+    id: 7,
     name: "A.P.O.D.",
     github: "https://github.com/Matthew-Hocking/A.P.O.D",
     description: "This React app uses the NASA A.P.O.D. API to return a new astronomical image everyday. In this solo project I learnt more about using external APIs that required hidden keys, and becoming familiar with SASS."
   },
   {
+    id: 8,
     name: "CatFacts",
     deployLink: "http://getcatfacts.herokuapp.com/",
     deployMethod: "HerokuApp",
@@ -53,6 +61,7 @@ export default [
     description: "CatFacts simply provides a random fact about cats, and with a click will return a new one. Being a solo project, this was my first opportunity at getting familiar with external APIs."
   },
   {
+    id: 9,
     name: "Matthew Hocking Blog",
     deployLink: "https://matthew-hocking.github.io/",
     deployMethod: "GH Pages",
