7f96964a094acac80601c89666a19d1f19704e88
matthew-hocking <matthocking2439@gmail.com>
2022-05-21 14:24:18 +1200

    refactoring skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,9 +9,9 @@ const Skills = () => {
     <div className='container full' id="skills">
       <Title prompt="Go through" title="My skills" />
       <div className='skills__card'>
-        <Grid container spacing={10}>
+        <Grid container spacing={6}>
           {skills.map((skill, id) => (
-            <Grid item key={id}>
+            <Grid item xs={4} md={4} lg={2} key={id}>
               <div className='skills__item'>
                 <img src={skill.image} alt={skill.name} />
                 <p>{skill.name}</p>
--- src/data/skills.js
+++ src/data/skills.js
@@ -1,42 +1,50 @@
 export default [
   {
-    id: 1,
     name: "JavaScript",
     image: "/images/javascript.svg"
   },
   {
-    id: 2,
     name: "HTML5",
     image: "/images/html5.svg"
   },
   {
-    id: 3,
     name: "CSS3",
     image: "/images/css3.svg"
   },
   {
-    id: 4,
     name: "Node",
     image: "/images/node.svg"
   },
   {
-    id: 5,
     name: "React",
     image: "/images/react.svg"
   },
   {
-    id: 6,
+    name: "Git",
+    image: "/images/git.svg"
+  },
+  {
     name: "SQLite3",
     image: "/images/sqlite.svg"
   },
   {
-    id: 7,
     name: "MongoDB",
     image: "/images/mongodb.svg"
   },
   {
-    id: 8,
     name: "Sanity CMS",
     image: "/images/sanity.svg"
-  }
+  },
+  {
+    name: "SASS",
+    image: "/images/sass.svg"
+  },
+  {
+    name: "Material UI",
+    image: "/images/material-ui.svg"
+  },
+  {
+    name: "",
+    image: ""
+  },
 ]
