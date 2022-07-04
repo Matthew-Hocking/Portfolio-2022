acd205df0e2b78173959c980c79f84f5bc128932
matthew-hocking <matthocking2439@gmail.com>
2022-07-04 20:19:31 +1200

    bye bye modal

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Grid } from '@mui/material'
 
 import Title from './Title'
-import ProjectModal from './ProjectModal'
 import projects from '../data/projects'
 
 const Projects = () => {
@@ -14,13 +13,20 @@ const Projects = () => {
       <div className='projects__card'>
         <Grid container spacing={2} >
           {projects.map((project, id) => (
-            <Grid item key={id} xs={4}>
-              <ProjectModal project={project} />
+            <Grid item lg={6}>
+              <div className='project' key={id}>
+                <div className='project__header'>
+                  <h2>{project.name}</h2>
+                  <div className='project__header_links'>
+                    <a href={project.github} target="_blank" >Github</a>
+                  </div>
+                </div>
+                <p>{ project.description}</p>
+              </div>
             </Grid>
           ))}
         </Grid>
       </div>
-
     </div>
   )
 }
